14d664ff03f86ad76b1cee1ecc22a2c4507d5600
Stiflerzak <hilarymwendia97@gmail.com>
2023-01-31 23:43:12 +0300

    Finished NavBar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import SearchIcon from "@mui/icons-material/Search";
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Container = styled.div`
   height: 60px;
@@ -26,16 +28,32 @@ const SearchContainer = styled.div`
   margin-left: 25px;
   padding: 5px;
 `;
+
+
 const Input = styled.input`
 border:none;
 `;
 
+const Logo= styled.h1`
+font-weight: bold; 
+`
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  text-align: center;
+  justify-content: flex-end;
 `;
+
+const MenuItem= styled.div`
+font-size: 14px;
+cursor; pointer;
+margin-left:25px;
+`
 
 export const Navbar = () => {
   return (
@@ -46,11 +64,21 @@ export const Navbar = () => {
 
           <SearchContainer>
             <Input />
-            <SearchIcon />
+            <SearchIcon style={{color:"gray", fontSize:16}}/>
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center> 
+        <Logo>HiShop</Logo>
+        </Center>
+        <Right>
+        <MenuItem>REGISTER</MenuItem>
+        <MenuItem>SIGN IN</MenuItem>
+        <MenuItem>
+        <Badge badgeContent={4} color="primary">
+        <ShoppingCartOutlinedIcon />
+      </Badge>
+        </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
