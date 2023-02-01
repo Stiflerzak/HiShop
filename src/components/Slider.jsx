194db625bd225ab567f683d871ce2f470bd11a27
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-01 15:56:10 +0300

    Added Styling to component Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,15 +5,14 @@ import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 const Container= styled.div`
     width: 100%;
     height: 100vh;
-    display: flex;
-    background-color: coral;
+    display: flex;   
     position: relative; 
 `
 const Arrow= styled.div`
 width=50px;
 height=50px;
 background-color: white;
-border-radius: 100%;
+border-radius: 50%;
 display: flex; 
 align-items: center;
 justify-content: center;
@@ -22,6 +21,8 @@ top:0;
 bottom:0;
 left: ${props=> props.direction === "left" && "10px"};
 right: ${props=> props.direction === "right" && "10px"};
+cursor: pointer;
+opacity: 0.5;
 margin:auto;
 `
 
