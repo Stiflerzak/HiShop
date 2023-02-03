f9e55d2a93aa7740699540e20ec69c99c5b18702
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-03 15:06:59 +0300

    Added more Slides feature

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,8 @@
+import React,{useState} from "react";
 import styled from "styled-components";
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from "../data";
 
 const Container= styled.div`
     width: 100%;
@@ -30,14 +32,14 @@ z-index: 2;
 const Wrapper= styled.div`
 height: 100%
 display:flex;
-transform: translateX(-100px)
+transform: translateX({props => props.slideIndex * -100}vw)
 `;
 const Slide= styled.div`
 width: 100vw;
 height: 100vh;
 display: flex;
 align-items: center;
-background-color:#${props=> props.bg}
+background-color:${props=> props.bg}
 `;
 
 const ImgContainer= styled.div`
@@ -72,46 +74,35 @@ cursor: pointer;
 export const Slider = () => {
     const [slideIndex, setSlideIndex] = useState(0);
 
-const handleClick= (direction) =>{}
+const handleClick= (direction) =>{
+
+    if(direction ==="left"){
+        setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+    } else {
+        setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
+}
 
   return (
     <Container>
     <Arrow direction= "left" onClick={()=> handleClick("left")}>
     <ArrowLeftOutlinedIcon/>
     </Arrow>
-<Wrapper>
-<Slide bg="#FF7F50">
-<ImgContainer> 
-<Image src="https://phonesstorekenya.com/wp-content/uploads/2021/09/Apple-iPhone-13-Pro-Max-b.jpg"/>
-</ImgContainer>
-<InfoContainer>
-<Title>Apple iPhone 13 Pro Max</Title>
-<Description>Don't compromise on quality! Get this 12MP + 12MP + 12MP + TOF 3D LiDAR</Description>
-<Button>SHOP NOW</Button>
-</InfoContainer>
-</Slide>
+<Wrapper slideIndex={slideIndex}>
+{sliderItems.map(item =>(
 
-<Slide bg="#BDB76B">
+<Slide bg={item.bg}>
 <ImgContainer> 
-<Image src="https://phonesstorekenya.com/wp-content/uploads/2021/05/Samsung-Galaxy-A22-5G.jpg"/>
+<Image src={item.img}/>
 </ImgContainer>
 <InfoContainer>
-<Title>Samsung Galaxy A22 5G</Title>
-<Description>48 MP + 8 MP + 5 MP</Description>
+<Title>{item.title}</Title>
+<Description>{item.description}</Description>
 <Button>SHOP NOW</Button>
 </InfoContainer>
 </Slide>
-
-<Slide bg="#BDB76B">
-<ImgContainer> 
-<Image src="https://phonesstorekenya.com/wp-content/uploads/2022/07/MacBook-Pro-13-inch-M2-chip-a.jpg"/>
-</ImgContainer>
-<InfoContainer>
-<Title>MacBook Pro 13-inch M2 2022</Title>
-<Description>13.3-inch, 2,560 x 1,600 Retina display 256 SSD, 512GB SSD</Description>
-<Button>SHOP NOW</Button>
-</InfoContainer>
-</Slide>
+  )
+  )}
 </Wrapper>
 
 
